ec6670165182ed64f3b6706ab8505e4692b2a2c9
Julio Cesar <jcrbarajas@gmail.com>
2021-11-09 22:13:55 -0700

    filtro pokemon

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import HomeCss from '../styles/Home.module.css'
@@ -5,13 +6,63 @@ import Link from 'next/link'
 
 
 
-export default function Home({ pokemones, minimimosDatos }) {
+export default function Home({ pokemones, minimimosDatos, tipos, notFound }) {
+
+
+  const [filtro, setFiltrar] = useState(minimimosDatos)
+
+  const filtrar = (elTipo) => {
+
+    setFiltrar(minimimosDatos)
+
+    if (elTipo === "borrar") {
+      setFiltrar(minimimosDatos)
+    }
+    else {
+
+      let filtradoPorTypo = minimimosDatos
+        .filter((pokemon) => pokemon.types.some((tipo) => tipo.type.name === elTipo))
+        .map((tem2) => {
+
+          let nuevosTem = { ...tem2 }
+
+          return nuevosTem
+        })
+      setFiltrar(filtradoPorTypo)
+
+    }
+
+  }
+
+
 
 
 
   return (
     <>
       <div className={HomeCss.container}>
+        <div className={HomeCss.filtros}>
+          <button className={`${HomeCss.botonFiltro} ${HomeCss.botonTodos}`} onClick={() => filtrar("borrar")}>
+            Mostrar todos
+          </button>
+          <div className={HomeCss.botones}>
+            {
+              tipos.map((tipo, index) => {
+
+                return (
+                  <button key={tipo.name} className={`${HomeCss.botonFiltro} ${tipo.name}`} aria-label={tipo.name} onClick={() => filtrar(tipo.name)}>
+
+                    {tipo.name}
+
+                  </button>
+                )
+              })
+            }
+
+
+          </div>
+        </div>
+
 
         <div className={HomeCss.titulo}>
           <h1>Pokemones</h1>
@@ -19,7 +70,7 @@ export default function Home({ pokemones, minimimosDatos }) {
         <div className={HomeCss.columnas}>
 
           <ul>
-            {minimimosDatos.map(pokemon => (
+            {filtro ? filtro.map(pokemon => (
               <li key={pokemon.id}>
                 <Link scroll={false} href={{
                   pathname: '/pokemon/[name]',
@@ -55,7 +106,7 @@ export default function Home({ pokemones, minimimosDatos }) {
 
                 </Link>
               </li>
-            ))}
+            )) : 'Cargando...'}
           </ul>
         </div>
 
@@ -65,16 +116,19 @@ export default function Home({ pokemones, minimimosDatos }) {
   )
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticProps(context) {
+
+  const resTipos = await fetch('https://pokeapi.co/api/v2/type')
+  const tipos = await resTipos.json()
 
   const traemosPokemones = async (porPokemon) => {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${porPokemon}/`)
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${porPokemon}?limit=101&offset=0/`)
     const data = await response.json()
 
     return data
   }
   let pokemones = []
-  for (let i = 1; i <= 20; i++) {
+  for (let i = 1; i <= 101; i++) {
     let data = await traemosPokemones(i)
     pokemones.push(data)
   }
@@ -90,10 +144,13 @@ export async function getServerSideProps(context) {
     }
   })
 
+
+
   return {
     props: {
+      tipos: tipos.results,
+      minimimosDatos,
 
-      minimimosDatos
     },
   }
 }
